206c86997a311008a89df5626210d7bffa9e801c
Shaikh Farooq <farooq@75f.io>
2019-09-10 22:33:07 +0530

    first commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Admin on 9/10/19.
+//  Copyright © 2019 Farooq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
