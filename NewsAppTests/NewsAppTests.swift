31dcfbf2f59ea41e242c1daec27beb7d88b144c9
Shaikh Farooq <farooq@75f.io>
2019-09-11 23:04:08 +0530

    added test case to check api success response

--- NewsAppTests/NewsAppTests.swift
+++ NewsAppTests/NewsAppTests.swift
@@ -30,5 +30,16 @@ class NewsAppTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testSearchWithMovieAvailable() {
+        let promise = expectation(description: "Fetch top headlines")
+        let url: String = "\(EndPoints.TopHeadline.path)\(EndPoints.Country.path)\(APIKey.ApiKey.rawValue)"
+        Network.shared.fetchNewsHeadlines(urlByName: url,type: NewsModel.self) { (response,success,error) in
+            XCTAssertNil(error)
+            XCTAssertTrue(success == "True")
+            promise.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
 
 }
